82f4c5dc0de620a6a56e49aed0b32e05ff1099aa
Paulo Gabriel <pgj1085@gmail.com>
2020-02-22 12:45:28 -0300

    Script adicionar produtos no body

--- web/js/home.js
+++ web/js/home.js
@@ -1,4 +1,6 @@
 let carrouselItems = document.querySelector("#corrossel-bonecos .carousel-inner");
+let producs = document.querySelector("#producs");
+
 let first = true;
 
 const addItemsCarousel = (img, link, altImg) => {
@@ -20,6 +22,52 @@ const addItemsCarousel = (img, link, altImg) => {
     carrouselItems.appendChild(divEl);
 }
 
+const addProducts = (img, altImg, title, desc, price) => {
+    let divEl = document.createElement("div");
+    let divCardEl = document.createElement("div");
+    let imgItemEl = document.createElement("img");
+    let divBodyEl = document.createElement("div");
+    let h5TitleEl = document.createElement("h5");
+    let pDescEl = document.createElement("p");
+    let divCartEl = document.createElement("div");
+    let divPriceEl = document.createElement("div");
+    let h6PriceEl = document.createElement("h6");
+    let divAddCartEl = document.createElement("div");
+    let imgAddCartEl = document.createElement("img");
+    divEl.setAttribute("class", "col-md-3 p-1");
+    divCardEl.setAttribute("class", "card col-12");
+    imgItemEl.setAttribute("src", img);
+    imgItemEl.setAttribute("alt", altImg);
+    imgItemEl.setAttribute("class", "card-img-top mt-2");
+    divBodyEl.setAttribute("class", "card-body");
+    h5TitleEl.setAttribute("class", "card-title");
+    h5TitleEl.innerHTML = title;
+    pDescEl.setAttribute("class", "card-text");
+    pDescEl.innerHTML = desc;
+    divCartEl.setAttribute("class", "add-cart row");
+    divPriceEl.setAttribute("class", "col-9");
+    h6PriceEl.innerHTML = "R$ " + price;
+    divAddCartEl.setAttribute("class", "col-3");
+    imgAddCartEl.setAttribute("src", "img/home/icons/126083.png");
+
+    divEl.appendChild(divCardEl);
+    divCardEl.appendChild(imgItemEl);
+    divCardEl.appendChild(divBodyEl);
+    divBodyEl.appendChild(h5TitleEl);
+    divBodyEl.appendChild(pDescEl);
+    divBodyEl.appendChild(divCartEl);
+    divCartEl.appendChild(divPriceEl);
+    divCartEl.appendChild(divAddCartEl);
+    divPriceEl.appendChild(h6PriceEl);
+    divAddCartEl.appendChild(imgAddCartEl);
+    producs.appendChild(divEl);
+}
+
 addItemsCarousel("img/home/bonec-selecao-1.png", "#", "selecao");
 addItemsCarousel("img/home/bonec-selecao-2.png", "#", "selecao");
 addItemsCarousel("img/home/boneco-palhaco.png", "#", "selecao");
+
+addProducts("img/home/produtos/item 1.webp", "Boneco", "Nome produto", "Descricao Produto", "999,99");
+addProducts("img/home/produtos/item 1.webp", "Boneco", "Nome produto", "Descricao Produto", "999,99");
+addProducts("img/home/produtos/item 1.webp", "Boneco", "Nome produto", "Descricao Produto", "999,99");
+addProducts("img/home/produtos/item 1.webp", "Boneco", "Nome produto", "Descricao Produto", "999,99");
