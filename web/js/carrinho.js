fb332fdde423fa7c9f2646389884a6016b89d528
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2020-02-22 11:06:34 -0300

    Adicionando o carrinho de compras

---
+++ web/js/carrinho.js
@@ -0,0 +1,11 @@
+let removerItem = document.querySelectorAll("#removeItem");
+
+
+
+removerItem.forEach(btn => btn.addEventListener("click", () => {
+    
+let node = event.target.parentNode.parentNode.parentNode.parentNode;
+
+node.remove();
+
+})); 
