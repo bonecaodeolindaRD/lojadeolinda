aa80751a885038427e0698c2c7936c7fc21e060e
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2020-02-23 17:28:30 -0300

    Arrumando calcular CEP fictício

--- web/js/detalharProduto.js
+++ web/js/detalharProduto.js
@@ -2,17 +2,42 @@ let btnCalcularFrete = document.getElementById("calcularFrete");
 let controles = document.getElementById("controle");
 let inputcepfrete = document.getElementById("cepFrete");
 
-
 btnCalcularFrete.addEventListener('click', () => {
 
-        let valor = inputcepfrete.value;
-        
-        let calcularFrete = ` <p class="mt-2">
-        <em> Receba em até  10 dias úteis 
-        <br> R$200,00
-        </em> 
-        </p>`;  
+        let frete = document.getElementById("frete");
+       
+        if (ehTamanho(inputcepfrete.value, 8)) {
 
-        controles.innerHTML += calcularFrete;
-    
+                //frete.setAttribute('class', 'mt-2 freteSuceso');
+                frete.textContent = `Receba em até  10 dias úteis
+        R$200,00 `;
+        
+        } else {
+                //frete.setAttribute('class', 'mt-2 freteErro');
+                frete.textContent = `O CEP informado não foi localizado `;
+        }
+
+
+});
+
+const ehTamanho = (valor, tamanho) =>{
+
+        return valor.length == tamanho;
+}
+
+
+inputcepfrete.addEventListener('keyup', (e) => {
+
+        if (isNaN(inputcepfrete.value)) {
+
+                inputcepfrete.value = inputcepfrete.value.substring(0, (inputcepfrete.value.length - 1));
+
+        }
+
+        if (inputcepfrete.value.length >= 8) {
+
+                inputcepfrete.value = inputcepfrete.value.substring(0, 8);
+
+        }
+
 });
