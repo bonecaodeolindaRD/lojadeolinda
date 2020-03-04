69416be38b951771d8188583cf43332a2d5266be
Treinamento <lucas-prieto@hotmail.com>
2020-03-04 15:59:45 -0300

    Editado o componente Login e adicionado imagens

--- web/react/src/components/Login/index.js
+++ web/react/src/components/Login/index.js
@@ -2,30 +2,32 @@ import React, { Component } from 'react';
 
 import { Card, Button, CardTitle, CardText, Row, Col, Container, InputGroup, InputGroupText, InputGroupAddon, Input} from 'reactstrap';
 
+import './styles.css';
+
 class Login extends Component{
     render(){
         return (
             <>
-            <Container align="center" justify-content="center">
-            <Row>
-      <Col xs="12" sm="6" md="6" >
-        <Card body>
-          <CardTitle>Login</CardTitle>
-          <InputGroup>
-          <InputGroupAddon addonType="prepend">
-          <InputGroupText>Email: </InputGroupText>
-          </InputGroupAddon>
-          <Input placeholder="user@mail.com" />
-          </InputGroup>
-          <br />
-          <InputGroup>
-          <InputGroupAddon addonType="prepend">
-          <InputGroupText>Senha: </InputGroupText>
-          </InputGroupAddon>
+            <Container className="tam" align="center" justify-content="center">
+            <Row className="tam align-items-center">
+              <Col xs="12" sm="6" md="6" >
+                <Card body>
+                  <CardTitle>Login</CardTitle>
+                    <InputGroup>
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText><img src="img/person.svg" alt="logo do site" width="20px" /> Email: </InputGroupText>
+                      </InputGroupAddon>
+                        <Input placeholder="user@mail.com" />
+                      </InputGroup>
+                      <br />
+                    <InputGroup>
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText><img src="img/key.svg" alt="logo do site" width="20px" /> Senha: </InputGroupText>
+                        </InputGroupAddon>
           <Input type="password" />
           </InputGroup>
           <div className="text-left">
-          <a href="/Senha" className="alert-link">Perdeu a senha ?</a>
+          <a href="/Senha" className="alert-link">Lembrar senha</a>
           </div>
           <br/>
           <div className="text-center">
