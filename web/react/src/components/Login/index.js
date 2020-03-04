28f24eaeca144eb1a663a0ea62a2c3f9db4abe48
Treinamento <lucas-prieto@hotmail.com>
2020-03-04 08:35:19 -0300

    Adicionado opção de lembrar senha e corrigido botões do Login

--- web/react/src/components/Login/index.js
+++ web/react/src/components/Login/index.js
@@ -24,17 +24,21 @@ class Login extends Component{
           </InputGroupAddon>
           <Input type="password" />
           </InputGroup>
-          <br />
+          <div className="text-left">
+          <a href="/Senha" className="alert-link">Perdeu a senha ?</a>
+          </div>
+          <br/>
           <div className="text-center">
               <Button size="md" color="success">Logar-se</Button>
-              </div>
+              <Button size="md" className="ml-3" href="/" color="danger">Cancelar</Button>
+          </div>
         </Card>
       </Col>
       <Col xs="12" sm="6" md="6">
         <Card body>
           <CardTitle>Cadastro</CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Cadastro</Button>
+          <CardText>Você não possui cadastro conosco ? Cadastre-se agora mesmo em nossa loja!</CardText>
+          <Button href="/Cadastro">Cadastro</Button>
         </Card>
       </Col>
     </Row>
