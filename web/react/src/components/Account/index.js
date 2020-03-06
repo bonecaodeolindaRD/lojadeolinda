2e6fc8b64a4892e5b7596c8c0c541c69d01d4df2
Treinamento <lucas-prieto@hotmail.com>
2020-03-06 09:12:23 -0300

    Adicionado Forms com Mask ao componente Account

--- web/react/src/components/Account/index.js
+++ web/react/src/components/Account/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
 import './styles.css';
+import InputMask from 'react-input-mask';
 
 import { Col, Row, Button, Form, FormGroup, Label, Input,Container } from 'reactstrap';
 
@@ -11,22 +12,47 @@ class Account extends Component{
             <>
             <Header/>
             <Container className="tam"  justify-content="center">
-                <Form>
-                    <Row form>
-                        <Col md={2}></Col>
-                        <Col md={4}>
-                            <FormGroup>
-                                <Label for="exampleEmail">Email</Label>
-                                <Input type="email" name="email" id="exampleEmail" placeholder="user@mail.com" />
-                            </FormGroup>
+                <Form text-align-center >
+                    <FormGroup row>
+                        <Col sm={3}></Col>
+                        <Label sm={1} for="nameUser">Nome: </Label>
+                        <Col sm={5}>
+                            <Input type="text" name="nameUser" id="nameUser" placeholder="Seu primeiro nome"/>
                         </Col>
-                        <Col md={4}>
-                            <FormGroup>
-                                <Label for="examplePassword">Senha</Label>
-                                <Input type="password" name="password" id="examplePassword" />
-                            </FormGroup>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Col sm={3}></Col>
+                        <Label sm={1} for="secNameUser">Sobrenome: </Label>
+                        <Col sm={5}>
+                            <Input type="text" name="secNameUser" id="SecNameUser" placeholder="Seu sobrenome"/>
                         </Col>
-                    </Row>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Col sm={3}></Col>
+                        <Label sm={1} for="Email">Email: </Label>
+                        <Col sm={5}>
+                            <Input type="email" name="email" id="Email" placeholder="user@mail.com" />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Col sm={3}></Col>
+                        <Label for="exampleSelect" sm={1}>Gênero: </Label>
+                            <Col sm={5}>
+                                <Input type="select" name="select" id="exampleSelect">
+                                    <option disable onSelect></option>
+                                    <option value="masculino">Masculino</option>
+                                    <option value="feminino">Feminino</option>
+                                    <option value="outros">Outros</option>
+                                </Input>
+                            </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Col sm={3}></Col>
+                        <Label sm={1} for="secNameUser">Contato: </Label>
+                        <Col sm={5}>
+                            <Input mask="(99) 99999-9999" maskChar="" id="cep" tag={InputMask} type="text" name="contact" id="contact" placeholder="Ex (11) 99999-9999"/>
+                        </Col>
+                    </FormGroup>
                 </Form>
             </Container>
             <Footer/>
