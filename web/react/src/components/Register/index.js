fe0a7cff287889922e24b9ef5737c6642343266c
Treinamento <lucas-prieto@hotmail.com>
2020-03-10 09:35:46 -0300

    Adicionado validações nos inputs do componente Register

--- web/react/src/components/Register/index.js
+++ web/react/src/components/Register/index.js
@@ -1,12 +1,76 @@
 import React, { Component } from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
+import api from "../../services/api";
 
 import InputMask from 'react-input-mask';
 
 import { Col, Form, FormGroup, Label, Input,Container  } from 'reactstrap';
 
 class Register extends Component{
+    constructor(props) {
+        super(props);
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeLastName = this.onChangeLastName.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangeGen = this.onChangeGen.bind(this);
+        this.onChangeContact = this.onChangeContact.bind(this);
+
+        this.state = {
+            NM_FIRSTNAME: "",
+            NM_LASTNAME: "",
+            EMAIL: "",
+            GEN: "",
+            CONTACT: "",
+
+        }
+    }
+
+    onChangeName(e){
+        this.setState({
+            NM_FIRSTNAME: e.target.value
+        })
+    }
+
+    onChangeLastName(e){
+        this.setState({
+            NM_LASTNAME: e.target.value
+        })
+    }
+
+    onChangeEmail(e){
+        this.setState({
+            EMAIL: e.target.value
+        })
+    }
+
+    onChangeGen(e){
+        this.setState({
+            GEN: e.target.value
+        })
+    }
+
+    onChangeContact(e){
+        this.setState({
+            CONTACT: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const obj = {
+          NM_FIRSTNAME: this.state.NM_FIRSTNAME,
+          NM_LASTNAME: this.state.NM_LASTNAME,
+          EMAIL: this.state.EMAIL,
+          GEN: this.state.GEN,
+          CONTACT: this.state.CONTACT,
+        };
+        api.post("/users", obj).then(res => console.log(res.data));
+
+        this.setState({
+
+        });
+    }
 
     render(){
         return(
@@ -21,33 +85,33 @@ class Register extends Component{
                 <h1>Registro de Conta</h1>
             </div>
             <br></br>
-                <Form text-align-center >
+                <Form onSubmit={this.onSubmit} text-align-center >
                     <FormGroup row>
                         <Col sm={3}></Col>
                         <Label sm={1} for="nameUser">Nome: </Label>
                         <Col sm={5}>
-                            <Input type="text" name="nameUser" id="nameUser" placeholder="Seu primeiro nome"/>
+                            <Input type="text" required name="nameUser" onChange={this.onChangeName} value={this.state.NM_FIRSTNAME} id="nameUser" placeholder="Seu primeiro nome"/>
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Col sm={3}></Col>
                         <Label sm={1} for="secNameUser">Sobrenome: </Label>
                         <Col sm={5}>
-                            <Input type="text" name="secNameUser" id="SecNameUser" placeholder="Seu sobrenome"/>
+                            <Input type="text" required name="secNameUser" onChange={this.onChangeLastName} value={this.state.NM_LASTNAME} id="SecNameUser" placeholder="Seu sobrenome"/>
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Col sm={3}></Col>
                         <Label sm={1} for="Email">Email: </Label>
                         <Col sm={5}>
-                            <Input type="email" name="email" id="Email" placeholder="user@mail.com" />
+                            <Input type="email" required name="email" onChange={this.onChangeEmail} value={this.state.EMAIL} id="Email" placeholder="user@mail.com" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Col sm={3}></Col>
-                        <Label for="exampleSelect" sm={1}>Gênero: </Label>
+                        <Label for="gen" sm={1}>Gênero: </Label>
                             <Col sm={5}>
-                                <Input type="select" name="select" id="exampleSelect">
+                                <Input type="select" required onChange={this.onChangeGen} value={this.state.GEN} name="gen" id="gen">
                                     <option disable onSelect></option>
                                     <option value="masculino">Masculino</option>
                                     <option value="feminino">Feminino</option>
@@ -57,23 +121,23 @@ class Register extends Component{
                     </FormGroup>
                     <FormGroup row>
                         <Col sm={3}></Col>
-                        <Label sm={1} for="secNameUser">Contato: </Label>
+                        <Label sm={1} for="contact">Contato: </Label>
                         <Col sm={5}>
-                            <Input mask="(99) 99999-9999" maskChar="" tag={InputMask} type="text" name="contact" id="contact" placeholder="Ex (11) 99999-9999"/>
+                            <Input mask="(99) 99999-9999" required maskChar="" onChange={this.onChangeContact} value={this.state.CONTACT} tag={InputMask} type="text" name="contact" id="contact" placeholder="Ex (11) 99999-9999"/>
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Col sm={3}></Col>
                         <Label sm={1} for="password">Senha: </Label>
                         <Col sm={5}>
-                            <Input  id="password" type="password" name="password"/>
+                            <Input  id="password" required type="password" name="password"/>
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Col sm={3}></Col>
                         <Label sm={1} for="confpassword">Confirmação: </Label>
                         <Col sm={5}>
-                            <Input  id="confpassword" type="password" name="confpassword"/>
+                            <Input  id="confpassword" required type="password" name="confpassword"/>
                         </Col>
                     </FormGroup>
                     <br/>
