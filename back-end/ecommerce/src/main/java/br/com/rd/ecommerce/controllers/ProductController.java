294d7cc36b4d9697fdd8377bf97a6fb9c6c2b876
Paulo Gabriel <pgj1085@gmail.com>
2020-03-11 14:39:55 -0300

    Adicionando atributos no pedido

--- back-end/ecommerce/src/main/java/br/com/rd/ecommerce/controllers/ProductController.java
+++ back-end/ecommerce/src/main/java/br/com/rd/ecommerce/controllers/ProductController.java
@@ -16,44 +16,50 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @PostMapping("create-product")
-    public Product save(@RequestBody Product product){
+    public Product save(@RequestBody Product product) {
         return productRepository.save(product);
     }
 
     @GetMapping("/product/all")
-    public List<Product> findAll(){
-        return productRepository.findAll();
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> products = productRepository.findAll();
+        if (products != null && products.size() > 0)
+            return ResponseEntity.ok().body(products);
+        return ResponseEntity.badRequest().build();
     }
 
     @GetMapping("/productId/{id}")
-    public ResponseEntity<Product> findById(@PathVariable("id") Long id){
+    public ResponseEntity<Product> findById(@PathVariable("id") Long id) {
         return ResponseEntity.ok().body(productRepository.findById(id).get());
     }
 
     @GetMapping("/productDescription/{descricao}")
-    public List<Product> findByName(@PathVariable("descricao") String name){
-        return productRepository.findByName(name);
+    public ResponseEntity<List<Product>> findByName(@PathVariable("descricao") String name) {
+        List<Product> products = productRepository.findByName(name);
+        if (products != null && products.size() > 0)
+            return ResponseEntity.ok().body(products);
+        return ResponseEntity.badRequest().build();
     }
 
     @GetMapping("/product")
-    public Product findProductById(@PathParam("id") Long id){
-        return productRepository.findById(id).stream().findFirst().orElse(null);
+    public ResponseEntity<Product> findProductById(@PathParam("id") Long id) {
+        return ResponseEntity.ok(productRepository.findById(id).get());
     }
 
     @DeleteMapping("/product/{id}")
-    public void deleteById(@PathVariable("id") Long id){
+    public void deleteById(@PathVariable("id") Long id) {
         productRepository.deleteById(id);
     }
 
     @PutMapping("/product")
-    public Product updateAll(@RequestBody Product product){
+    public ResponseEntity<Product> updateAll(@RequestBody Product product) {
         Product p = productRepository.findById(product.getId()).get();
         p.setCategory(product.getCategory());
         p.setDescription(product.getDescription());
         p.setImage(product.getImage());
         p.setName(product.getName());
         p.setPrice(product.getPrice());
-        return productRepository.save(p);
+        return ResponseEntity.ok().body(productRepository.save(p));
     }
 
 }
