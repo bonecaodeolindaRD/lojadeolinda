c6534e3b44d3ee551673a620b6189871dac8b948
Treinamento <Treinamento@RD723900.raiadrogasil.com>
2020-03-11 17:18:35 -0300

    Adicionando StockProduct

--- back-end/ecommerce/src/main/java/br/com/rd/ecommerce/controllers/StockController.java
+++ back-end/ecommerce/src/main/java/br/com/rd/ecommerce/controllers/StockController.java
@@ -1,16 +1,21 @@
 package br.com.rd.ecommerce.controllers;
 
 import br.com.rd.ecommerce.models.entities.Stock;
+import br.com.rd.ecommerce.repositories.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StockController {
 
-//    @Autowired
-//    private StockRepository stockRepository;
-//
-//    @PostMapping("create-stock")
-//    public Stock
+    @Autowired
+    private StockRepository stockRepository;
+
+    @PostMapping("/create-stock")
+    public Stock save(@RequestBody Stock stock){
+        return  stockRepository.save(stock);
+    }
 }
