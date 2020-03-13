fdfffd49cac101c8d6b80adabdb7b152578f4a5c
Treinamento <Treinamento@RD723937.raiadrogasil.com>
2020-03-13 10:54:16 -0300

    criada a pag de compra concluida e remoção da pag listProduct

--- web/react/src/routes.js
+++ web/react/src/routes.js
@@ -8,8 +8,8 @@ import Contact from './components/Contact';
 import About from './components/About';
 import Checkout from './components/Checkout';
 import Cart from './components/Cart';
-import List from './components/ProductsList';
 import ProductDetail from './components/ProductDetail';
+import Success from './components/Success';
 import { isAuthenticated } from "./services/auth";
 
 
@@ -35,9 +35,10 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
             <Route path="/register" exact component={Register} />
             <Route path="/about" exact component={About}/>
             <Route path="/cart" exact component={Cart}/>
-            <Route path="/list" exact component={List}/>
+          
             <Route path="/login" exact component={Login} />
-            <Route path="/detalhe/:id" exact component={ProductDetail} />            
+            <Route path="/detalhe/:id" exact component={ProductDetail} />   
+            <Route path="/success" exact component={Success}/>         
             <PrivateRoute path="/account" exact component={Account} />
             <PrivateRoute path="/checkout" exact component={Checkout} />
         </Switch>
