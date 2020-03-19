c77f9a321efd3f6c2f56fb7bfb4a6a8f13458662
Paulo Gabriel <pgj1085@gmail.com>
2020-03-19 08:25:39 -0300

    Pequena correcao no checkout caso aconteca algum erro na hora de gerar o pedido

--- web/react/src/components/Checkout/index.js
+++ web/react/src/components/Checkout/index.js
@@ -122,6 +122,7 @@ export default class Checkout extends Component {
             let {data: returnAddress } = await axios.post("http://localhost:8080/ecommerce/address/new", address);
             if(!returnAddress){
                 this.setState({ erro: "Erro ao gerar o pedido" });
+                this.submeted = false;
                 return false;
             }
             let obj = {
@@ -149,6 +150,7 @@ export default class Checkout extends Component {
             let { data: order } = await axios.post("http://localhost:8080/ecommerce/order/new", obj);
             if(!order){
                 this.setState({ erro: "Erro ao gerar o pedido" });
+                this.submeted = false;
                 return false;
             }
             sessionStorage.setItem('order', JSON.stringify(order));
