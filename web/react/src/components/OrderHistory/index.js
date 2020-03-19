2adcbde0bbbfb289a58bc31ce8d8095c35a61c8a
Lucas Prieto de Senna <lucas-prieto@hotmail.com>
2020-03-19 10:29:02 -0300

    Alterações no OrderHistory

--- web/react/src/components/OrderHistory/index.js
+++ web/react/src/components/OrderHistory/index.js
@@ -59,7 +59,7 @@ class OrderHistory extends Component {
                                         {(order.value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
                                     </td>
                                     <td>
-                                        {order.date}
+                                        {(order.date)}
                                     </td>
                                     <td>
                                         {(order.shipping).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
