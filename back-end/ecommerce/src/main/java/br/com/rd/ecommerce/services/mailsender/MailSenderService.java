c4d7a7a5b27c883259e8f02f30467556098bd680
Paulo Gabriel <pgj1085@gmail.com>
2020-03-19 13:45:02 -0300

    Tratando excecao do envio de email

--- back-end/ecommerce/src/main/java/br/com/rd/ecommerce/services/mailsender/MailSenderService.java
+++ back-end/ecommerce/src/main/java/br/com/rd/ecommerce/services/mailsender/MailSenderService.java
@@ -1,7 +1,9 @@
 package br.com.rd.ecommerce.services.mailsender;
 
+import org.springframework.mail.MailSendException;
+
 public interface MailSenderService {
 
-    String sendMail(String to, String from, String text, String subject);
+    void sendMail(String to, String from, String text, String subject) throws MailSendException;
 
 }
