8ce5b5ca7b752734e0ad5f2ac06dbb499b5fde6e
Paulo Gabriel <pgj1085@gmail.com>
2020-03-20 10:32:12 -0300

    Layout da pagina de cadastro do produto no estoque feita

--- dashboard/src/components/AddItemToStock/index.js
+++ dashboard/src/components/AddItemToStock/index.js
@@ -7,7 +7,9 @@ import {
   Col,
   Input,
   Button,
-  Form
+  Label,
+  Form,
+  FormGroup
 } from 'reactstrap';
 
 export default class AddItemToStock extends Component {
@@ -23,25 +25,41 @@ export default class AddItemToStock extends Component {
   render() {
     return (
       <>
-        <Header/>
+        <Header />
         <Container className="mt-5 border">
           <Form className="m-2">
             <Row>
               <Col xs={3}>
-                <Input type="select" id="stocks" name="stocks" defaultValue="0">
-                  <option value="0">Selecione um estoque</option>
-                  {this.state.stocks.map(s => (
-                    <option value={s.id}>{s.name}</option>
-                  ))}
-                </Input>
+                <FormGroup>
+                  <Label for="stocks">Estoques: </Label>
+                  <Input type="select" id="stocks" name="stocks" defaultValue="0">
+                    <option value="0">Selecione um estoque</option>
+                    {this.state.stocks.map(s => (
+                      <option value={s.id}>{s.name}</option>
+                    ))}
+                  </Input>
+                </FormGroup>
               </Col>
-              <Col xs="9">
-                <Input type="select" id="products" name="products" defaultValue="0">
-                  <option value="0">Selecione um produto</option>
-                  {this.state.products.map(p => (
-                    <option value={p.id}>{p.name}</option>
-                  ))}
-                </Input>
+              <Col xs={3}>
+                <FormGroup>
+                  <Label for="products">Produtos: </Label>
+                  <Input type="select" id="products" name="products" defaultValue="0">
+                    <option value="0">Selecione um produto</option>
+                    {this.state.products.map(p => (
+                      <option value={p.id}>{p.name}</option>
+                    ))}
+                  </Input>
+                </FormGroup>
+              </Col>
+              <Col xs={3}>
+                <FormGroup>
+                  <Label for="quantity">Quantidade a adicionar: </Label>
+                  <Input type="number" id="quantity" name="quanatity" min={0} />
+                </FormGroup>
+              </Col>
+              <Col xs={3}>
+                <Label>Finalizar</Label>
+                <Button color="success" type="submit" className="form-control">Adicionar</Button>
               </Col>
             </Row>
           </Form>
