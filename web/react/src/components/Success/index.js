4e1de3facc34fc760c1279282bd4c7d8710911cf
Treinamento <Treinamento@RD723937.raiadrogasil.com>
2020-03-20 10:57:06 -0300

    concertando id do pedido success

--- web/react/src/components/Success/index.js
+++ web/react/src/components/Success/index.js
@@ -21,26 +21,34 @@ export default class Success extends Component {
             this.props.history.push('/');
             return;
         }
+
+        this.clearLocal();
         this.getProducts();
-    } 
+    }
 
     clearLocal = () => {
         let { id } = JSON.parse(sessionStorage.getItem('order'));
         this.id = id;
-         sessionStorage.removeItem('cart');
-         sessionStorage.removeItem('order');
+        // sessionStorage.removeItem('cart');
+        // sessionStorage.removeItem('order');
     }
 
     getProducts = async () => {
 
         let productsItem = await JSON.parse(sessionStorage.getItem('cart'));
-  
+    
+
+        
+
+        if (productsItem === null) {
+            //this.props.history.push("/");
+            return
+        }
         this.state.products.forEach(p => productsItem.push({
             id: p.id,
             image: p.image,
             name: p.name,
             desc: p.description,
-
             value: p.value,
             quantity: p.quantity
             
@@ -48,7 +56,9 @@ export default class Success extends Component {
 
         
         this.setState({ products: productsItem });
-        this.clearLocal();
+
+
+
     }
 
     componentDidMount() {
