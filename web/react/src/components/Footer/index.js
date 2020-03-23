0f92034eb372beee5ae6b512b5655dd22a96fb58
Treinamento <Treinamento@RD723937.raiadrogasil.com>
2020-03-23 15:54:46 -0300

    Arrumando footer

--- web/react/src/components/Footer/index.js
+++ web/react/src/components/Footer/index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import {
     Navbar,
     Nav,
-    NavItem
+    NavItem,
+    Col,
+    Row
 } from 'reactstrap';
 
 import {
@@ -19,80 +21,112 @@ export default class Footer extends Component {
     render() {
         return (
             <Navbar color="dark" className=" mt-5">
+
                 <Nav>
-                    <NavItem className="space-footer">
-                        <img src="https://i.imgur.com/5RAN6zL.png" alt="logo do site" className="img-logo" />
-                        <small className="d-block mb-3 text-muted">&copy; Bonecão de Olinda</small>
-                    </NavItem>
-                    <NavItem className="space-footer">
+                    
+                    <Col className="col-2">
+                    
+                        <NavItem className="space-footer">
+                        
+                            <img src="https://i.imgur.com/5RAN6zL.png" alt="logo do site" className="img-logo" />
+                            <small className="d-block mb-3 text-muted">&copy; Bonecão de Olinda</small>
+                        
+                        </NavItem>
+                       
+                    </Col>
+                     
+                    <Col className="col-3 mr-2">
+                        <NavItem className="space-footer">
+                            <Nav vertical>
+                                <NavItem className="text-white">
+                                    <Link to="/" className="text-white" disabled>Pagamento</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link className="text-muted" to="/">Cartão de credito</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link className="text-muted" to="/">Cartão de débito</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link className="text-muted" to="/"><img src="https://www.valeriowagner.com.br/wp-content/uploads/2019/07/Cartao-que-aceitamos.jpg" width="120px" height="60px" alt="cartoes" /></Link>
+                                </NavItem>
+                            </Nav>
+                        </NavItem>
+                    </Col>
+
+                    <Col className="col-3 mr-2">
+                        <NavItem className="space-footer">
+                            <Nav vertical>
+                                <NavItem className="text-white">
+                                    <Link to="/" className="text-white" disabled>Redes sociais</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link className="text-muted" to="/"><FaFacebook /> <h6>Bonecão de Olinda</h6></Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link className="text-muted" to="/"><FaInstagram /> <h6>@Bonecão_de_Olinda</h6></Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link className="text-muted" to="/"><FaTwitter /> <h6>@Bonecão_de_Olinda</h6></Link>
+                                </NavItem>
+                            </Nav>
+
+                        </NavItem>
+                    </Col>
+
+                    <Col className="col-2">
+
+                        <NavItem className="space-footer">
+                            <Nav vertical>
+                                <NavItem>
+
+                                    <Link to="/" className="text-white" disabled>Contato</Link>
+                                </NavItem>
+                            </Nav>
+                        </NavItem>
+
+                   
+                    <NavItem>
                         <Nav vertical>
-                            <NavItem className="text-white">
-                                <Link to="/" className="text-white" disabled>Pagamento</Link>
-                            </NavItem>
                             <NavItem>
-                                <Link className="text-muted" to="/">Cartão de credito</Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link className="text-muted" to="/">Cartão de débito</Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link className="text-muted" to="/"><img src="https://www.valeriowagner.com.br/wp-content/uploads/2019/07/Cartao-que-aceitamos.jpg" width="120px" height="60px" alt="cartoes"/></Link>
-                            </NavItem>
-                        </Nav>
-                    </NavItem>
-                    <NavItem className="space-footer">
-                        <Nav vertical>
-                            <NavItem className="text-white h-5">
-                                <Link to="/" className="text-white" disabled>Redes sociais</Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link className="text-muted" to="/"><FaFacebook /> <h6> Bonecão de Olinda </h6></Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link className="text-muted" to="/"><FaInstagram /> <h6> @Bonecão_de_Olinda </h6></Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link className="text-muted" to="/"><FaTwitter /> <h6> @Bonecão_de_Olinda </h6></Link>
-                            </NavItem>
-                     </Nav>
-
-                     </NavItem>
-
-                            <NavItem className="space-footer">
-                                <Nav vertical>
-                                    <NavItem>
-
-                                        <Link to="/" className="text-white" disabled>Contato</Link>
-                                    </NavItem>
-
-                                    <NavItem>
-                                        <Link className="text-muted" to="/contact">
-                                            Entrar em contato
+                                <Link className="text-muted" to="/contact">
+                                    Entrar em contato
                                    </Link>
 
-                                    </NavItem>
-                                </Nav>
                             </NavItem>
-                            
-                            <NavItem className="space-footer">
 
-                                <div>
-                                    <Link to="/" className="text-white" disabled>Sobre</Link>
-                                </div>
-
-                                <div>
-                                    <Link className="text-muted" to="/about">
-                                        Nossa História
-                        </Link>
-                                </div>
+                            <NavItem>
+                                <Link className="text-muted" to="/contact">
+                                    Ligue (11) 93537-4847
+                                   </Link>
 
                             </NavItem>
                         </Nav>
-                       
+                    </NavItem>
+                    </Col>
 
-                       </Navbar>
+                    <Col>
 
-                    
-                 )  
-                }
-            }
+                    <NavItem className="col-2">
+                      <Row>
+                        <div>
+                            <Link to="/" className="text-white" disabled>Sobre</Link>
+                        </div>
+
+                        <div>
+                            <Link className="text-muted" to="/about">
+                                Nossa História
+                        </Link>
+                        </div>
+                       </Row>
+                    </NavItem>
+                </Col>
+                </Nav>
+               
+
+            </Navbar >
+
+
+        )
+    }
+}
