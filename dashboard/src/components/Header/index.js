5029c0d5bf8ad6b03cc3de153eb48ac1d3ae21c3
Paulo Gabriel <pgj1085@gmail.com>
2020-03-23 14:03:14 -0300

    Arrumando header dashboard

--- dashboard/src/components/Header/index.js
+++ dashboard/src/components/Header/index.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
 
 import {
-    NavLink
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  Container,
+  NavLink
 } from 'reactstrap';
+
+import { Link } from 'react-router-dom';
 
 
 export default class Header extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
   render() {
     return (
-        <>
-            <NavLink className="navbar navbar-dark bg-dark">
-                    <NavLink className="navbar-brand" href="#">Adicione um novo produto</NavLink>
-            </NavLink>
-        </>
+      <div>
+        <Navbar color="light" light expand="md">
+          <Container fluid>
+            <NavbarBrand href="/">Bonecão de olinda dashboard</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem>
+                  <NavLink>
+                    <Link to="/">Inicio</Link>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink>
+                    <Link to="/create">Criar um produto</Link>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink>
+                    <Link to="/add">Adicionar produto no estoque</Link>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink>
+                    <Link to="/sales">Vendas</Link>
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
     )
   }
 }
