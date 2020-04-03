6f7e90cfa567841de3e97143986a0dd4bcd50c47
Paulo Gabriel <pgj1085@gmail.com>
2020-04-02 21:32:34 -0300

    Removendo repeticao de codigo API ecomm

--- back-end/ecommerce/src/main/java/br/com/rd/ecommerce/services/stock/StockServiceImpl.java
+++ back-end/ecommerce/src/main/java/br/com/rd/ecommerce/services/stock/StockServiceImpl.java
@@ -67,29 +67,5 @@ public class StockServiceImpl implements StockService {
         return ResponseEntity.ok().body(returnStock);
     }
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> handlerEntityExceptionException(EntityNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex);
-    }
-
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity<?> handlerSQLException(SQLException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
-    }
-
-    @ExceptionHandler(JDBCException.class)
-    public ResponseEntity<?> handlerJDBCException(JDBCException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
-    }
-
-    @ExceptionHandler(SQLGrammarException.class)
-    public ResponseEntity<?> handlerSQLGrammarException(SQLGrammarException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
-    }
 
 }
